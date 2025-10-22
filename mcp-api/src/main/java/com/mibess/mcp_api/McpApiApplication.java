5743405ad33cbb463594238cbe26b2f061243fd3
Mibess <c.silverio@live.com>
2025-10-22 20:58:00 -0300

    first commit for MCP server

---
+++ mcp-api/src/main/java/com/mibess/mcp_api/McpApiApplication.java
@@ -0,0 +1,13 @@
+package com.mibess.mcp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpApiApplication.class, args);
+	}
+
+}
